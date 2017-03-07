26f569edaab857333b4bc4d69c4311a68503199c
Ataraxia Co., Ltd <AtaraxiaCoLtd@gmail.com>
2017-03-07 15:31:36 +0900

    CVSS dataset scrape complete

--- cve_report.go
+++ cve_report.go
@@ -7,11 +7,13 @@ import (
 
 	"github.com/yhat/scrape"
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 const (
-	CVSS3_ID = "BodyPlaceHolder_cplPageContent_plcZones_lt_zoneCenter_VulnerabilityDetail_VulnFormView_Vuln3CvssPanel"
 	NVD_URI  = "https://web.nvd.nist.gov/view/vuln/detail?vulnId=%s"
+	CVSS3_ID = "BodyPlaceHolder_cplPageContent_plcZones_lt_zoneCenter_VulnerabilityDetail_VulnFormView_Vuln3CvssPanel"
+	CVSS2_ID = "BodyPlaceHolder_cplPageContent_plcZones_lt_zoneCenter_VulnerabilityDetail_VulnFormView_Vuln2CvssPanel"
 )
 
 func main() {
@@ -20,9 +22,8 @@ func main() {
 	flag.StringVar(&flag_cve, "number", "CVE-2017-6074", "cve numbers")
 	flag.StringVar(&flag_cve, "n", "CVE-2017-6074", "cve numbers")
 
-	// debug messages
 	fmt.Printf("[+] Generate for %s\n", flag_cve)
-	fmt.Printf("[+] Scrape from "+NVD_URI, flag_cve)
+	fmt.Printf("[+] Scrape from "+NVD_URI+"\n", flag_cve)
 
 	// request and parse the front page
 	resp, err := http.Get(fmt.Sprintf(NVD_URI, flag_cve))
@@ -35,5 +36,37 @@ func main() {
 		panic(err)
 	}
 
+	// Scrape CVSS3 from NVD
 	CVSS3, ok := scrape.Find(root, scrape.ById(CVSS3_ID))
+	if ok {
+		fmt.Printf("[+] Detect CVSS3 dataset\n")
+		matcherList := func(n *html.Node) bool {
+			if n.DataAtom == atom.Div {
+				return scrape.Attr(n, "class") == "row"
+			}
+			return false
+		}
+
+		CVSS3Node := scrape.FindAll(CVSS3, matcherList)
+		for _, node := range CVSS3Node {
+			fmt.Printf("%s\n", scrape.Text(node))
+		}
+	}
+
+	// Scrape CVSS2 from NVD
+	CVSS2, ok := scrape.Find(root, scrape.ById(CVSS2_ID))
+	if ok {
+		fmt.Printf("[+] Detect CVSS2 dataset\n")
+		matcherList := func(n *html.Node) bool {
+			if n.DataAtom == atom.Div {
+				return scrape.Attr(n, "class") == "row"
+			}
+			return false
+		}
+
+		CVSS2Node := scrape.FindAll(CVSS2, matcherList)
+		for _, node := range CVSS2Node {
+			fmt.Printf("%s\n", scrape.Text(node))
+		}
+	}
 }
